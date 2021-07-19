edc6611fdd5814f3a2f69dbe9d197bdb9a2bdf94
juhyunham <judev0227@gmail.com>
2021-07-19 23:35:13 +0900

    delete unused file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+function Food({fav}) {
+  return <h1>I like {fav}</h1>
+}
+
+const foodILike = [`kimchi`, `ramen`, `bibimbap`, `kimbab`]
+
+function renderFood(dish) {
+  return <Food fav={dish.name}/>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello!!!</h1>
+      {foodILike.map(renderFood)}
+    </>
   );
 }
 
